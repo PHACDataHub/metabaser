5107c78719145cf2dfae5b69367664eae1cf1b2e
Jean-Pierre Mirenzi <jean-pierre.mirenzi@canada.ca>
2020-09-11 10:20:17 -0400

    Architechture change to use a handle object to manage API connection.

--- R/metabaser.R
+++ R/metabaser.R
@@ -2,15 +2,29 @@
 NULL
 
 #' Assemble URL
+#'
+#' @param handle metabase_handle object
+#' @param path API endpoint path
 #' @keywords internal
-build_url <- function(base, path) {
-    paste(base, path, sep = "/")
+build_url <- function(handle, path) {
+    paste(handle$base_url, path, sep = "/")
 }
 
+#' Construct a Metabase API Handle
+#'
+#' This is constructor to make a metabase_handle object which
+#' is used in subsequent functions to interact with the API.
+#' The handle should not be created manually, but should be created
+#' using \code{\link{metabase_login}}.
+#'
+#' @param base_url Base URL for the Metabase API
+#' @param database_id Database ID to connect to
+#' @param username Username
+#' @keywords internal
 metabase_handle <- function(base_url, database_id, username) {
     structure(list(
         base_url = base_url,
-        database_id = database_id,
+        database_id = as.integer(database_id),
         username = username,
         handle = httr::handle(base_url),
         token = NA,
@@ -18,42 +32,10 @@ metabase_handle <- function(base_url, database_id, username) {
     ), class = "metabase_handle")
 }
 
-#' Get/Set Metabase URL
-#'
-#' If no argument is given, retrieves the current Metabase URL.
-#' If an argument is given, sets the Metabase URL.
-#'
-#' @export
-metabase_url <- function(url = NULL) {
-    if (!is.null(url)) {
-        Sys.setenv(METABASE_BASE_URL = url)
-        return(invisible(url))
-    } else {
-        url <- Sys.getenv("METABASE_BASE_URL", unset = NA)
-        if (is.na(url)) {
-            warning("env var METABASE_BASE_URL is not set to the Metabse API.", call. = FALSE)
-        }
-    }
-    url
-}
-
-#' Get/Set Metabase DB ID
-#'
-#' If no argument is given, retrieves the current Metabase database ID.
-#' If an argument is given, sets the Metabase database ID.
-#'
-#' @export
-metabase_db <- function(db_id = NULL) {
-    if (!is.null(db_id)) {
-        Sys.setenv(METABASE_DATABASE_ID = db_id)
-        return(invisible(db_id))
-    } else {
-        db_id <- Sys.getenv("METABASE_DATABASE_ID", unset = NA)
-        if (is.na(db_id)) {
-            stop("env var METABASE_DATABASE_ID is not set to the ID number of the database.", call. = FALSE)
-        }
-    }
-    db_id
+#' @describeIn metabase_handle Check for metabase_handle
+#' @keywords internal
+is_metabase_handle <- function(handle) {
+    inherits(handle, "metabase_handle")
 }
 
 #' Login to Metabase
@@ -62,8 +44,8 @@ metabase_db <- function(db_id = NULL) {
 #' Credentials must be specified either directly through the \code{username} and \code{password} parameters
 #' or as a file through the \code{creds_file} parameter.
 #'
-#' The file should have "username=username" on one line and "password=password" on the next.
-#' If the login is successful, a cookie will be set behind the scenes with a session ID for all future requests.
+#' If used, the creds_file should have "username=username" on one line and "password=password" on the next.
+#' If the login is successful, a \code{metabase_handle} object will be returned to use in subsequent functions.
 #'
 #' @param base_url Base URL for the Metabase API
 #' @param database_id Database ID to connect to
@@ -72,14 +54,6 @@ metabase_db <- function(db_id = NULL) {
 #' @param password Password
 #' @export
 metabase_login <- function(base_url, database_id, creds_file = NULL, username = NULL, password = NULL) {
-    if (metabase_status()) {
-        warning("Already logged-in to Metabase. Please logout before trying to login.", call. = FALSE)
-        return(invisible(NULL))
-    }
-
-    ###metabase_url(base_url)
-    ###metabase_db(database_id)
-
     if (!is.null(creds_file)) {
         creds <- stringr::str_split(readr::read_lines(creds_file), "=", simplify = TRUE)[,2]
         username <- creds[1]
@@ -91,74 +65,59 @@ metabase_login <- function(base_url, database_id, creds_file = NULL, username = 
         stop("One of creds_file or username and password must be specified.", call. = FALSE)
     }
 
-    hand <- metabase_handle(base_url = base_url, database_id = database_id, username = username)
+    handle <- metabase_handle(base_url = base_url, database_id = database_id, username = username)
 
     resp <- httr::POST(
-        url = build_url(base_url, path = "session"),
+        url = build_url(handle, path = "session"),
         body = list(username = username, password = password),
         encode = "json",
-        handle = hand$handle
+        handle = handle$handle
     )
 
     if (httr::http_error(resp)) {
-        stop(
-            stringr::str_glue(
-                "Metabase login failed [{httr::status_code(resp)}]",
-                unlist(httr::content(resp)$errors),
-                .sep = "\n"
-            ),
-        call. = FALSE)
+        stop(stringr::str_glue("Metabase login failed [{httr::status_code(resp)}]",
+                               unlist(httr::content(resp)$errors),
+                               .sep = "\n"), call. = FALSE)
     }
-    hand$token <- hand$handle %>%
+
+    handle$token <- handle$handle %>%
         httr::cookies() %>%
         dplyr::filter(name == "metabase.SESSION") %>%
         dplyr::pull(value)
-    hand$status <- TRUE
+    handle$status <- TRUE
     message(stringr::str_glue("Metabase login successful for {username} !"))
-    hand
+    handle
 }
 
 #' Logout of Metabase
 #'
 #' Logs out the user by sending a request to delete the user session.
+#'
+#' @param handle metabase_handle object
 #' @export
-metabase_logout <- function() {
-    if (!metabase_status()) {
+metabase_logout <- function(handle) {
+    handle_name <- deparse(substitute(handle))
+    stopifnot(is_metabase_handle(handle))
+    if (!handle$status) {
         warning("Cannot logout. There is no active Metabase session.", call. = FALSE)
         return(invisible(NULL))
     }
 
     resp <- httr::DELETE(
-        url = build_url(path = "session")
+        url = build_url(handle, path = "session"),
+        handle = handle$handle
     )
 
     if (httr::http_error(resp)) {
-        stop(
-            stringr::str_glue(
-                "Metabase logout failed [{httr::status_code(resp)}]",
-                httr::content(resp, type = "text", encoding = "UTF-8"),
-                .sep = "\n"
-            )
-        )
+        stop(stringr::str_glue("Metabase logout failed [{httr::status_code(resp)}]",
+                               httr::content(resp, type = "text", encoding = "UTF-8"),
+                               .sep = "\n"), call. = FALSE)
     } else {
-        message(
-            "Metabase logout successful!"
-        )
+        message("Metabase logout successful!")
+        handle$status <- FALSE
+        assign(handle_name, handle, pos = 1) # update handle in-place in parent env with updated status
     }
-}
-
-#' Check Metabase status
-#'
-#' @return TRUE if a session is active with a logged-in user, FALSE otherwise.
-#' @export
-metabase_status <- function() {
-    base_url <- suppressWarnings(metabase_url())
-    session_id <- httr::handle_find(base_url) %>%
-        httr::cookies() %>%
-        dplyr::filter(name == "metabase.SESSION") %>%
-        dplyr::pull(value)
-    if (length(session_id) == 0) return(FALSE)
-    !is.na(session_id)
+    invisible(handle) # return invisibly
 }
 
 #' Query Metabase
@@ -167,30 +126,22 @@ metabase_status <- function() {
 #' Data is limited to 2000 records by the server using this approach.
 #' This might be faster for small lookups of the DB.
 #'
+#' @param handle metabase_handle object
 #' @param sql_query SQL query to execute
-#' @param database_id Database ID to query, will be set automatically upon login
 #'
 #' @return data.frame containing the results of the query
 #' @export
-metabase_query2 <- function(sql_query, database_id = metabase_db()) {
-    if (!metabase_status()) stop("No connection to Metabase.")
-    database_id <- as.integer(database_id)
+metabase_query2 <- function(handle, sql_query) {
     resp <- httr::POST(
-        url = build_url(path = "dataset"),
-        body = list(database = database_id,
+        url = build_url(handle, path = "dataset"),
+        body = list(database = handle$database_id,
                     native = list(query = sql_query),
                     type = "native"),
         encode = "json"
     )
     parsed <- jsonlite::fromJSON(httr::content(resp, as = "text", encoding = "UTF-8"))
     if (parsed$status == "failed") {
-        stop(
-            stringr::str_glue(
-                "Metabase query failed.",
-                parsed$error,
-                .sep = "\n"
-            )
-        )
+        stop(stringr::str_glue("Metabase query failed.", parsed$error, .sep = "\n"), call. = FALSE)
     } else if (parsed$status == "completed") {
         data <- parsed$data$rows
         colnames(data) <- parsed$data$cols$name
@@ -204,39 +155,36 @@ metabase_query2 <- function(sql_query, database_id = metabase_db()) {
 #' Data is limited to 1 million records by the server using this approach.
 #' This might be faster for small lookups of the DB.
 #'
+#' @param handle metabase_handle object
 #' @param sql_query SQL query to execute
 #' @param col_types Column types to use for parsing as specified in \code{\link[readr]{read_csv}}
-#' @param database_id Database ID to query, will be set automatically upon login
 #'
 #' @return data.frame containing the results of the query
 #' @export
-metabase_query <- function(sql_query, col_types = NULL, database_id = metabase_db()) {
-    if (!metabase_status()) stop("No connection to Metabase.")
-    database_id <- as.integer(database_id)
+metabase_query <- function(handle, sql_query, col_types = NULL) {
+    stopifnot(is_metabase_handle(handle))
+    if (!handle$status) stop("No connection to Metabase. Try logging in again.", call. = FALSE)
+
     resp <- httr::POST(
-        url = build_url(path = "dataset/csv"),
+        url = build_url(handle, path = "dataset/csv"),
         body = list(query = jsonlite::toJSON(list(
-            database = database_id,
+            database = handle$database_id,
             native = list(query = sql_query),
             type = "native"),
             auto_unbox = TRUE)),
         encode = "form",
-        httr::content_type("application/x-www-form-urlencoded")
+        config = list(httr::content_type("application/x-www-form-urlencoded")),
+        handle = handle$handle
     )
+
     # api will return json instead of csv if there is an error so check if we have json
     resp_type <- ifelse(rawToChar(httr::content(resp, as = "raw")[1]) == "{", "json", "csv")
     if (resp_type == "json") {
         parsed <- jsonlite::fromJSON(httr::content(resp, as = "text", encoding = "UTF-8"))
         if (parsed$status == "failed") {
-            stop(
-                stringr::str_glue(
-                    "Metabase query failed.",
-                    parsed$error,
-                    .sep = "\n"
-                )
-            )
+            stop(stringr::str_glue("Metabase query failed.", parsed$error, .sep = "\n"), call. = FALSE)
         } else {
-            stop("Incorrect format returned by Metabase (json instead of csv).")
+            stop("Incorrect format returned by Metabase (json instead of csv).", call. = FALSE)
         }
     } else if (resp_type == "csv") {
         readr::read_csv(httr::content(resp, as = "text", encoding = "UTF-8"), col_types = col_types)
